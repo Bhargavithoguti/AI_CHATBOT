14bd8d3d96e50e571c77581e5539348ddfdbc3bd
unknown <bbthoguti@gmail.com>
2026-09-15 21:30:29 +0530

    changed gemini model2

--- Backend/services/geminiService.js
+++ Backend/services/geminiService.js
@@ -7,7 +7,7 @@ const ai = new GoogleGenAI({
 const generateAIReply = async (message) => {
   try {
     const response = await ai.models.generateContent({
-      model: "gemini-3.5-flash",
+      model: "gemini-2.5-flash",
 
       config: {
         systemInstruction: `
